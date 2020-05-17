c42cf790e31ea6b0cf3bce7a9ad83f5e16a67e64
lkilli <58042708+lisekilli@users.noreply.github.com>
2020-05-17 23:56:15 +0200

    Add files via upload

---
+++ :js:question1.js
@@ -0,0 +1,25 @@
+const form = document.querySelector("#contactForm");
+form.addEventListener("submit", validateForm);
+function validateForm(event) {
+    event.preventDefault(); 
+    console.log("The form was submitted"); 
+
+    const firstName = document.querySelector("#firstName");
+    const firstNameError = document.querySelector("firstNameError");
+    const firstNameValue = firstName.Value; 
+
+    if (checkInputLength(firstNameValue) === true) {
+        firstNameError.style.display = "none";
+    } else {
+        firstNameError.style.display = "block";
+    }
+}
+
+function checkInputLength(value) {
+    const trimmedValue = value.trim();
+    if (trimmedValue.length > 2) {
+        return true; 
+    } else {
+        return false;
+    }
+}
